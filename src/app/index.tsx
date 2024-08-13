b300b6fc71da2dc0aa40d4c6b91cdb3aae1ce508
Danilo Romão <htpps.sw@gmail.com>
2024-08-13 00:26:34 -0300

    commit inital

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import "../styles/global.css"
+
+export default function Index() {
+     return (
+          <>
+               <View >
+                    <Text> Hello LU</Text>
+               </View>
+          </>
+     )
+}
